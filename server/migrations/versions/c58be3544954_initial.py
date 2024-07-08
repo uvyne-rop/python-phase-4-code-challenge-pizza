f38d1c410a81ca246faf547e17d1fec4f4d3ee17
uvyne-rop <ropchiri7@gmail.com>
2024-07-08 08:35:20 +0300

    passed all the tests and the application is functional

---
+++ server/migrations/versions/c58be3544954_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: c58be3544954
+Revises: 170fb498ef9a
+Create Date: 2024-07-07 21:01:38.613474
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c58be3544954'
+down_revision = '170fb498ef9a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
